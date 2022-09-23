d57d5c02da194b639f314eacff5f02793d93c233
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-09-23 21:01:20 +0900

    add: create question table and its routing and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('question', QuestionController::class);
+});
 
 Route::get('/', function () {
     return view('welcome');
